5de8e0f752330f0a8634dd330eb25329d25c4a28
ikede-divine-keno <ikededivine@gmail.com>
2023-10-05 09:23:48 -0700

    0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# Write a Bash script that sets up your web servers for the deployment of web_static.
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+    sudo ufw allow 'Nginx HTTP'
+fi
+
+if [ ! -d /data/ ]; then
+    sudo mkdir /data/
+fi
+
+if [ ! -d /data/web_static/ ]; then
+    sudo mkdir /data/web_static/
+fi
+
+if [ ! -d /data/web_static/releases/ ]; then
+    sudo mkdir /data/web_static/releases/
+fi
+
+if [ ! -d /data/web_static/shared/ ]; then
+    sudo mkdir /data/web_static/shared/
+fi
+
+if [ ! -d /data/web_static/releases/test/ ]; then
+    sudo mkdir /data/web_static/releases/test/
+fi
+myhtml="<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <title>My HTML Page</title>
+    </head>
+    <body>
+        <p>This is a HTML Page</p>
+    </body>
+</html>
+"
+
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "$myhtml" > /data/web_static/releases/test/index.html
+
+if [ -f /data/web_static/current ]; then
+    sudo rm -rf /data/web_static/current
+fi
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "s|server_name _;|server_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\ttry_files \$uri \$uri/ =404;\n\t}|" /etc/nginx/sites-enabled/default
+sudo nginx -t
+sudo service nginx restart
