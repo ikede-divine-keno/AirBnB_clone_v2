44c34b66f7c2d8c265a2d6bc8f0e4ae979d436fb
ikede-divine-keno <ikededivine@gmail.com>
2023-10-20 07:56:18 -0700

    2-c_route.py added

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Write a script that runs a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Display Hello HBNB! upon GET request"""
+    return f"Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display HBNB upon GET request"""
+    return f"HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Display C followed by various input text upon GET request"""
+    return f"C %s" % escape(text).replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
