b0c9dcfaf8417104ad059a04fc9f41c92993e666
ikede-divine-keno <ikededivine@gmail.com>
2023-10-20 05:18:59 -0700

    0-hello_route.py modified

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
+"""Write a script that starts a Flask web application"""
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """display Hello HBNB! after GET request"""
     return f"Hello HBNB!"
 
 
