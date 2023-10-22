fcab74901088380d6a478a75c84c8a5bfb2e7f8a
ikede-divine-keno <ikededivine@gmail.com>
2023-10-22 09:49:00 -0700

    test

--- models/state.py
+++ models/state.py
@@ -14,8 +14,8 @@ class State(BaseModel, Base):
     if os.environ.get("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade="all, delete")
-
     else:
+
         @property
         def cities(self):
             """Return the cities"""
