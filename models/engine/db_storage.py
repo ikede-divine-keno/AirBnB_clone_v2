eed10f9e2152a53aaff9010f75794bd1275656cd
ikede-divine-keno <ikededivine@gmail.com>
2023-10-22 09:42:44 -0700

    pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -46,17 +46,20 @@ class DBStorage:
                 cls = eval(cls)
             queries = self.__session.query(cls).all()
             for query in queries:
-                del query._sa_instance_state
-                objs.update({"{}.{}".format(query.__class__.__name__, query.id): query})
+                # del query._sa_instance_state
+                objs.update({"{}.{}".format(
+                    query.__class__.__name__, query.id): query}
+                    )
             return objs
         else:
             classes = [User, City, State, Place, Review, Amenity]
             for class_ in classes:
                 queries = self.__session.query(class_).all()
                 for query in queries:
-                    del query._sa_instance_state
+                    # del query._sa_instance_state
                     objs.update(
-                        {"{}.{}".format(query.__class__.__name__, query.id): query}
+                        {"{}.{}".format
+                            (query.__class__.__name__, query.id): query}
                     )
             return objs
 
