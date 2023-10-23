043005cbd0356ff22c3307b5d15416ddfb6c784a
ikede-divine-keno <ikededivine@gmail.com>
2023-10-22 18:49:09 -0700

    100-dump.sql, web_flask/100-hbnb.py, web_flask/templates/100-hbnb.html and web_flask/static/ added

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+from flask import Flask, render_template
+
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb")
+def hbnb():
+    """hbnb function"""
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
+    places = storage.all("Place").values()
+    return render_template(
+        "100-hbnb.html", states=states, amenities=amenities, places=places
+    )
+
+
+@app.teardown_appcontext
+def teardown(err):
+    """teardown app context"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
